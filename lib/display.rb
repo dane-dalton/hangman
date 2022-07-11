738e9c7880cb6a257e99fd6f0e62a25d97cb61dd
Dane Dalton <danezdalton1@gmail.com>
2022-07-11 13:28:59 -0500

    Welcome message - playing board - game over check - guess input

--- lib/display.rb
+++ lib/display.rb
@@ -1,11 +1,33 @@
 module Display
-  def attempts
 
+  def welcome
+    puts "Welcome to Hangman! Guess letters to figure out the computer's word.\n"
   end
 
-  def score
-
+  def board(correct, word, guesses)
+    puts "You have #{correct} misses left...\n"
+    word.each do |letter|
+      print "#{letter} "
+    end
+    puts "\n\n"
+    puts "Your current guesses are: "
+    guesses.each do |guess|
+      print "#{guess} "
+    end
+    puts "\n\n"
   end
 
-  
+  def make_guess
+    invalid = true
+    puts "Guess a letter: "
+    while (invalid) do
+      letter = gets.chomp.downcase
+      if letter == /[a-z]/
+        invalid = false
+      else
+        puts "Please input a single character."
+      end
+    end
+    return letter
+  end
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,14 +1,37 @@
-class Game
+require_relative 'display.rb'
 
-  attr_accessor :word_bank
+class Game
+  include Display
+
+  attr_accessor :secret_word, :incorrect_counter, :guess_bank, :current_word
 
   def initialize()
     @word_bank = File.open('dictionary.txt').readlines.map { |line| line.chomp }
-    self.word_bank = self.word_bank.select { |word| word.length >= 5 && word.length <= 12 }
+    @modified_bank = @word_bank.select { |word| word.length >= 5 && word.length <= 12 }
+    @secret_word = @modified_bank[rand(@modified_bank.length)].split('')
+    @incorrect_counter = 6
+    @guess_bank = []
+    @current_word = @secret_word.dup.map { |letter| "_" }
   end
 
+  def play
+    welcome()
+    until (game_over?(self.incorrect_counter))
+      board(self.incorrect_counter, self.current_word, self.guess_bank)
+      make_guess()
+      
+    end
+  end
   
+  def game_over?(counter)
+    if counter <= 0
+      true
+    else
+      false
+    end
+  end
+
 end
 
 game = Game.new()
-p game.word_bank
+puts game.play
